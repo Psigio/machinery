ecdcbe6115733d2e15df2b4618467ac91cb1a612
Jon <Psigio@users.noreply.github.com>
2020-12-29 14:32:11 +0000

    Update External-BLE-Range.cmake
    Add glib libraries to list of required linker items.

--- cmake/External-BLE-Range.cmake
+++ cmake/External-BLE-Range.cmake
@@ -20,6 +20,13 @@ add_dependencies(BLE-Range project_BLE-Range)
 set(BLE-RANGE_INCLUDE_DIR ${CMAKE_BINARY_DIR}/BLE-Range/lib/ ${CMAKE_BINARY_DIR}/BLE-Range/include/)
 set(BLE-RANGE_LIBRARY_DIR ${CMAKE_BINARY_DIR}/BLE-Range/lib/ ${CMAKE_BINARY_DIR}/BLE-Range/lib/)
 
-set(BLE-RANGE_LIBRARIES libBLE-Range.a)
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(GLIB REQUIRED glib-2.0)
+pkg_check_modules (GOBJECT REQUIRED gobject-2.0)
+pkg_check_modules (GIO REQUIRED gio-2.0)
+set(THREADS_PREFER_PTHREAD_FLAG ON)
+FIND_PACKAGE(Threads REQUIRED)
+
+set(BLE-RANGE_LIBRARIES libBLE-Range.a ${GLIB_LIBRARIES} ${GOBJECT_LIBRARIES} ${GIO_LIBRARIES} Threads::Threads)
 
 include_directories(${BLE-RANAGE_INCLUDE_DIR})
